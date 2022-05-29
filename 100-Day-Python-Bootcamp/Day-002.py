3a7d1f2df7835fd8e6d50f5d307551a252593e9f
Aaron <peacock.aaron@protonmail.com>
2022-05-29 18:27:34 -0400

    Edit Day-002.py and Day-003.py

---
+++ 100-Day-Python-Bootcamp/Day-002.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator.")
+total = input("What was the total bill? $")
+tip = input("What percentage tip would you like to give? 10, 12 or 15? ")
+peeps = input("How many people to split the bill? ")
+amount = round((float(total) / int(peeps)) * 1.12,2)
+amount = "{:.2f}".format(amount)
+print(f"Each person should pay: ${amount}")
