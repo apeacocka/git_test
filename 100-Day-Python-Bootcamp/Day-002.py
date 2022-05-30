1185fc910d54dcf7704acb1284c7c1ea6f3c1883
Aaron <peacock.aaron@protonmail.com>
2022-05-30 14:29:03 -0400

    Edit Day-002.py

--- 100-Day-Python-Bootcamp/Day-002.py
+++ 100-Day-Python-Bootcamp/Day-002.py
@@ -5,3 +5,12 @@ peeps = input("How many people to split the bill? ")
 amount = round((float(total) / int(peeps)) * 1.12,2)
 amount = "{:.2f}".format(amount)
 print(f"Each person should pay: ${amount}")
+
+print("\n")
+print("\n")
+print("\n")
+
+
+
+
+
