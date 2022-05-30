c47b291a1855cfcf2f2b0481c23ac124c38ab5ef
Aaron <peacock.aaron@protonmail.com>
2022-05-29 22:39:17 -0400

    Edit Day-001.py

--- 100-Day-Python-Bootcamp/Day-001.py
+++ 100-Day-Python-Bootcamp/Day-001.py
@@ -5,5 +5,15 @@ a = input("What is the name of your birth city?\n")
 #3. Ask the user for the name of a pet.
 b = input("\nWhat is your pets name?\n")
 #4. Combine the name of their city and pet and show them their band name.
-print("\nYour band name should be " + a + " " + b)
+print("\nYour band name should be " + a + " " + b + "\n")
 #5. Make sure the input cursor shows on a new line, see the example at:
+
+print("The big switch\n")
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
+
