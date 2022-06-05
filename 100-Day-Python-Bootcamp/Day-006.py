0681557b7c95beffadf7bb18e2fa64df3d96ca1f
Aaron <peacock.aaron@protonmail.com>
2022-06-05 17:55:21 -0400

    Edit Day-006.py

--- 100-Day-Python-Bootcamp/Day-006.py
+++ 100-Day-Python-Bootcamp/Day-006.py
@@ -9,3 +9,51 @@ def my_function():
     print("Bye")
 #To trigger or call the function, type the name
 my_function()
+
+#Robot Game
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# move()
+# move()
+# turn_around
+# move()
+# move()
+# turn_around
+
+#Make a box
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+
+#Hurdle
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# for step in range(6:
+#     hurdle()
