712c4ff148d7e08a8c51f5348b89b8bb34a75066
Aaron <peacock.aaron@protonmail.com>
2022-06-06 18:11:39 -0400

    Edit Day-007.py

---
+++ 100-Day-Python-Bootcamp/Day-008.py
@@ -0,0 +1,17 @@
+#Functions that allow you to give them input
+#Arguments and Parameters
+
+#Functions take a complex set of instructions and puts them inside a block of code that is named
+#Call the function to execute the code fucntion()
+
+#def my_fucntion():
+#   do this
+#   then do this
+#   finally do this
+
+def my_function():
+    print("Monkey")
+    print("Leopard")
+    print("Giraffe")
+
+my_function()
