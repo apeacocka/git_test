5b1dc4dd60c6080dd242b0364a1c99882ef30fa3
Aaron <peacock.aaron@protonmail.com>
2022-07-12 19:18:29 -0400

    Edit Day-019.py

--- 100-Day-Python-Bootcamp/Day-019.py
+++ 100-Day-Python-Bootcamp/Day-019.py
@@ -38,10 +38,10 @@ import random
 
 is_race_on = False
 screen = Screen()
-screen.setup(width=500, height=400)
+screen.setup(width=1000, height=800)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_positions = [-70, -40, -10, 20, 50, 80]
+y_positions = [-140, -80, -20, 40, 100, 160]
 all_turtles = []
 
 
@@ -49,7 +49,7 @@ for turtle_index in range(0, 6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.color(colors[turtle_index])
     new_turtle.penup()
-    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle.goto(x=-480, y=y_positions[turtle_index])
     all_turtles.append(new_turtle)
 
 if user_bet:
@@ -60,7 +60,7 @@ while is_race_on:
 
 
     for turtle in all_turtles:
-        if turtle.xcor() > 230:
+        if turtle.xcor() > 480:
             is_race_on = False
             winning_color = turtle.pencolor()
             if winning_color == user_bet:
