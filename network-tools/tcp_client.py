cdf250aadc8d67bd2a71dd3d0c1ca6d3f689d112
Aaron <peacock.aaron@protonmail.com>
2022-07-23 22:03:41 -0400

    Edit Day-020.pynetwork-tools/tcp_client.py

--- network-tools/tcp_client.py
+++ network-tools/tcp_client.py
@@ -1,6 +1,6 @@
 import socket
-target_host = "0.0.0.0"
-target_port = 9998
+target_host = "www.google.com"
+target_port = 80
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client
